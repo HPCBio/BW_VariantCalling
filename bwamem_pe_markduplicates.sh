59136fe6642fd7ba56ef24a14c109f4dc9e7af37
Gloria Rendon <rendong@ncsa.illinois.edu>
2015-08-16 19:15:06 -0500

    adding QC to bwa_mem_pe_markduplicates

--- bwamem_pe_markduplicates.sh
+++ bwamem_pe_markduplicates.sh
@@ -42,7 +42,7 @@ echo "##################################### PARSING RUN INFO FILE ##############
 echo "##################################### AND SANITY CHECK      ########################################"
 echo "####################################################################################################"
 
-
+rootdir=$( cat $runfile | grep -w OUTPUTDIR | cut -d '=' -f2 )
 javadir=$( cat $runfile | grep -w JAVADIR | cut -d "=" -f2 )
 picardir=$( cat $runfile | grep -w PICARDIR | cut -d "=" -f2 )
 markduptool=$( cat $runfile | grep -w MARKDUPLICATESTOOL | cut -d "=" -f2 | tr '[a-z]' '[A-Z]' )
@@ -56,6 +56,9 @@ alignparms=$( cat $runfile | grep -w BWAMEMPARAMS | cut -d '=' -f2 )
 memprof=$( cat $runfile | grep -w MEMPROFCOMMAND | cut -d '=' -f2 )
 header=$( echo $RGparms  | tr ":" "\t" )
 rgheader=$( echo -n -e "@RG\t" )$( echo -e "${header}"  | tr "=" ":" )
+dup_cutoff=$( cat $runfile | grep -w DUP_CUTOFF | cut -d '=' -f2 )
+map_cutoff=$( cat $runfile | grep -w MAP_CUTOFF | cut -d '=' -f2 )
+novodir=$( cat $runfile | grep -w NOVODIR | cut -d '=' -f2 )
 
 if [ ! -d $picardir ]
 then
@@ -176,43 +179,6 @@ then
             cp $qsubfile $AlignOutputLogs/FAILEDjobs/
             exit 1;
         fi        
-
-
-	#header=$( echo "${header}"  | sed "s/\t/\tRG/g" | sed "s/ID\=/RGID\=/" )
-
-        #$javadir/java -Xmx1024m -Xms1024m -jar $picardir/AddOrReplaceReadGroups.jar \
-        #     INPUT=${bamprefix}.tmp.sam \
-        #     OUTPUT=${bamprefix}.tmp_wrg.sam \
-        #     TMP_DIR=$outputdir \
-        #     SORT_ORDER=coordinate \
-        #     MAX_RECORDS_IN_RAM=null \
-        #     CREATE_INDEX=true \
-        #     VALIDATION_STRINGENCY=SILENT \
-        #     $header
-        
-        #ln -s ${bamprefix}.tmp.sam ${bamprefix}.tmp_wrg.sam
-        #exitcode=$?
-        #echo `date`
-        #if [ $exitcode -ne 0 ]
-        #then
-        #    MSG="picard-addorreplacereadgroups commands failed on ${bamprefix}.sorted.bam  exitcode=$exitcode. alignment failed"
-	#    echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" 
-        #    echo -e "program=$0 failed at line=$LINENO.\nReason=$MSG\n$LOGS" >> $AlignOutputLogs/FAILEDmessages
-        #    cp $qsubfile $AlignOutputLogs/FAILEDjobs/
-        #    exit $exitcode;
-        #fi
-
-        #$samdir/samtools view -bS ${bamprefix}.tmp_wrg.sam >  ${bamprefix}.tmp.bam
-        #exitcode=$?
-        #echo `date`
-        #if [ $exitcode -ne 0 ]
-        #then
-            #MSG="samtools sam2bam conversion  failed on ${bamprefix}.tmp.sam  exitcode=$exitcode. alignment failed"
-	    #echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" 
-            #echo -e "program=$0 failed at line=$LINENO.\nReason=$MSG\n$LOGS" >> $AlignOutputLogs/FAILEDmessages
-            #cp $qsubfile $AlignOutputLogs/FAILEDjobs/
-            #exit $exitcode;
-        #fi
         
         $novodir/novosort --index --tmpdir $outputdir --threads $threads -m 16g --kt --compression 1 -o ${bamprefix}.sorted.bam ${bamprefix}.tmp.bam
         exitcode=$?
@@ -240,9 +206,176 @@ then
         #######################################################################################################
         #######################################################################################################
         ## here we insert the code for performing QC with percent mapped reads ... as in mergeLanesPerSample.sh
+        ## but only for Baylor samples
+        #######################################################################################################
+        #######################################################################################################
+
+
+        if [ -s $rootdir/SAMPLENAMES_multiplexed.list ]
+        then
+             echo "#######################################################################################################"
+             echo "########    This is a Baylor sample. We need to perform QC and write results to a txt file      #######"
+             echo "#######################################################################################################"
+             echo "#######################################################################################################"
+             echo "########   QC rules: duplication cutoff <= $dup_cutoff AND mapped_reads cutoff >= $map_cutoff   #######"
+             echo "########                                                                                        #######"
+             echo "#######################################################################################################"
+
+             ### create the output file for the results of QC and reset variables            
+             QCfile=$rootdir/QC_Results.txt
+             QCresult=""
+
+             ### generate the files with the pertinent stats
+             prestats=${bamprefix}.sorted.bam.flagstat
+             poststats=${bamprefix}.sorted.bam.properlyMapped.bam.flagstat
+             
+             $samdir/samtools flagstat ${bamprefix}.sorted.bam > $prestats
+             $samdir/samtools view -bu -F 12 ${bamprefix}.sorted.bam > ${bamprefix}.sorted.bam.properlyMapped.bam 
+             $samdir/samtools flagstat  ${bamprefix}.sorted.bam.properlyMapped.bam > $poststats
+
+
+             ####### making sure that stats were produced 
+             if [ ! -s $prestats ]
+             then
+ 	         MSG="samtools flagstat command produced an empty file with ${bamprefix}.sorted.bam"
+ 	         echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" #| ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+ 	         exit $exitcode;
+             fi
+             if [ ! -s $poststats ]
+              then
+ 	         MSG="samtools flagstat command produced an empty file with ${bamprefix}.sorted.bam.properlyMapped.bam"
+ 	         echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" #| ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+ 	         exit $exitcode;
+             fi
+
+  	     echo "#####################################################################################################"
+ 	     echo "#####################################################################################################"
+             echo "             now extracting the stats of interest from the  flagstat files "
+ 	     echo "#####################################################################################################"
+ 	     echo "#####################################################################################################"
+           
+
+             tot_mapped=$( cat $poststats | grep "mapped (" | cut -d ' ' -f1 )
+             tot_reads=$( cat $prestats | grep "in total" | cut -d ' ' -f1 )
+             tot_dups=$( cat $poststats | grep "duplicates" | cut -d ' ' -f1 )
+
+             #now testing if these variables have numbers
+
+             if [ $tot_dups -eq $tot_dups 2>/dev/null ]
+             then
+               echo -e "ok val"
+             else
+	       MSG="$prestats samtools flagstat file parsed incorrectly"
+	       echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" #| ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       #echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       exit $exitcode;
+	     fi
+             if [ $tot_reads -eq $tot_reads 2>/dev/null ]
+             then
+               echo -e "ok val"
+             else
+	       MSG="$prestats samtools flagstat file parsed incorrectly"
+	       echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" #| ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       #echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       exit $exitcode;
+	     fi
+
+             if [ $tot_mapped -eq $tot_mapped 2>/dev/null ]
+             then
+               echo -e "ok val"
+             else
+	       MSG="$prestats samtools flagstat file parsed incorrectly"
+	       echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" #| ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       #echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       exit $exitcode;
+             fi
+
+ 	     echo "#####################################################################################################"
+ 	     echo "#####################################################################################################"
+             echo "      now calculating percentages to be used for the filtering step and making sure they have numbers"
+ 	     echo "#####################################################################################################"
+ 	     echo "#####################################################################################################"
+           
+
+             perc_dup=$(( tot_dups * 100 / tot_reads ))
+             perc_mapped=$(( tot_mapped * 100 / tot_reads ))
+             
+             #now testing if these variables have numbers
+             if [ $perc_dup -eq $perc_dup 2>/dev/null ]
+             then
+               echo -e "ok val"
+             else
+	       MSG="$stats samtools flagstat file parsed incorrectly"
+	       echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" #| ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       #echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       exit $exitcode;
+             fi
+
+             if [ $perc_mapped -eq $perc_mapped 2>/dev/null ]
+             then
+               echo -e "ok val"
+             else
+	       MSG="$stats samtools flagstat file parsed incorrectly"
+	       echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" #| ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       #echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | ssh iforge "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+	       exit $exitcode;
+             fi
+
+ 	     echo "#####################################################################################################"
+ 	     echo "#####################################################################################################"
+             echo "                     now applying the filters and populating the output reports  "
+ 	     echo "#####################################################################################################"
+ 	     echo "#####################################################################################################"
+
+
+             if [ $perc_dup -lt $dup_cutoff ]
+             then
+               echo -e "${bamprefix}.sorted.bam passed first filter percent_duplicates with value $perc_dup, maximum cutoff is $dup_cutoff"
+               if [ $perc_mapped -gt $map_cutoff ]
+               then
+                  echo -e "${bamprefix}.sorted.bam passed second filter percent_mapped with value $perc_mapped, minimum cutoff is $map_cutoff"
+                  detail="${bamprefix}.sorted.bam\tPASSED\tpercent_duplication=$perc_dup\tduplication_cutoff=$dup_cutoff\tpercent_mapped=$perc_mapped\tmapping_cutoff=$map_cutoff"
+	          echo -e "$detail" >> $QCfile
+	          QCresult="PASSED"
+               else
+                  echo -e "${bamprefix}.sorted.bam DID NOT pass second filter percent_mapped value $perc_mapped, minimum cutoff is $map_cutoff"
+                  detail="${bamprefix}.sorted.bam\tFAILED\tpercent_duplication=$perc_dup\tduplication_cutoff=$dup_cutoff\tpercent_mapped=$perc_mapped\tmapping_cutoff=$map_cutoff"
+	          echo -e "$detail"  >> $QCfile
+	          QCresult="FAILED"	          
+               fi
+             else
+               echo -e "${bamprefix}.sorted.bam DID NOT pass first filter percent_duplicates value $perc_dup, minimum cutoff is $dup_cutoff"
+               detail="${bamprefix}.sorted.bam\tFAILED\tpercent_duplication=$perc_dup\tduplication_cutoff=$dup_cutoff\tpercent_mapped=$perc_mapped\tmapping_cutoff=$map_cutoff"
+	       echo -e "$detail"  >> $QCfile
+	       QCresult="FAILED"
+             fi
+
+
+ 	     echo "#####################################################################################################"
+             echo "                     now using value of QCresult to decide whether to stop execution or not          "
+ 	     echo "#####################################################################################################"
+ 	     echo "#####################################################################################################"
+
+             if [ $QCresult != "PASSED" ]
+             then
+                 MSG="${bamprefix}.sorted.bam  failed QC test. Analysis will stop now"
+	         echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" 
+                 echo -e "program=$0 failed at line=$LINENO.\nReason=$MSG\n$LOGS" >> $AlignOutputLogs/FAILEDmessages
+                 cp $qsubfile $AlignOutputLogs/FAILEDjobs/
+                 exit $exitcode;
+             fi
+
+        fi  # end of QC of Baylor sample
+        
+ 
         #######################################################################################################
         #######################################################################################################
         
+        #######################################################################################################
+        ########    This is a Baylor sample. Done with QC, now we perform markduplicates                #######
+        #######################################################################################################
+ 
+ 
         $javadir/java -Xmx8g -Xms1024m -jar $picardir/MarkDuplicates.jar \
              INPUT=${bamprefix}.sorted.bam \
              OUTPUT=${bamprefix}.wdups \
@@ -257,23 +390,23 @@ then
         echo `date`
         if [ $exitcode -ne 0 ]
         then
-            MSG="picard-Markduplicates commands failed on ${bamprefix}.sorted.bam  exitcode=$exitcode. alignment failed"
-	    echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" 
-            echo -e "program=$0 failed at line=$LINENO.\nReason=$MSG\n$LOGS" >> $AlignOutputLogs/FAILEDmessages
-            cp $qsubfile $AlignOutputLogs/FAILEDjobs/
-            exit $exitcode;
+                 MSG="picard-Markduplicates commands failed on ${bamprefix}.sorted.bam  exitcode=$exitcode. alignment failed"
+	         echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" 
+                 echo -e "program=$0 failed at line=$LINENO.\nReason=$MSG\n$LOGS" >> $AlignOutputLogs/FAILEDmessages
+                 cp $qsubfile $AlignOutputLogs/FAILEDjobs/
+                 exit $exitcode;
         fi
         if [ ! -s ${bamprefix}.wdups ]
         then
-            MSG="${bamprefix}.wdups aligned file not created. alignment failed"
-	    echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" 
-	    #echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | ssh iforge  "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
-            echo -e "program=$0 failed at line=$LINENO.\nReason=$MSG\n$LOGS" >> $AlignOutputLogs/FAILEDmessages
-            cp $qsubfile $AlignOutputLogs/FAILEDjobs/
-            exit 1;
-        fi        
+                 MSG="${bamprefix}.wdups aligned file not created. alignment failed"
+	         echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" 
+	         #echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | ssh iforge  "mailx -s '[Support #200] variant identification pipeline' "$redmine,$email""
+                 echo -e "program=$0 failed at line=$LINENO.\nReason=$MSG\n$LOGS" >> $AlignOutputLogs/FAILEDmessages
+                 cp $qsubfile $AlignOutputLogs/FAILEDjobs/
+                 exit 1;
+        fi
+fi # end of PICARD case
 
-fi
 echo -e "#################      WRAP UP: sort by coordinate and index on the fly: required for creating an indexed bam, ###############"
 echo -e "#################      which in turn is required for extracting alignments by chromosome                       ###############"
 
