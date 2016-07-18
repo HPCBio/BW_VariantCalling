dc567c1202ea332263f932e7b29201afdd1bb209
zo0z <azzaea@uofk.edu>
2016-07-18 12:20:15 -0500

    fitching the right file from the IndelDir

--- realign_varcall_by_chr.sh
+++ realign_varcall_by_chr.sh
@@ -234,10 +234,10 @@ echo -e "###  because they are specified differently for each GATK tool         
 cd $indel_local
 
 
-recalparmsindels=$( find ${PWD} -name "*${chr}*.vcf" | sed "s/^/ --knownSites /g" | tr "\n" " " )
+recalparmsindels=$( find ${PWD} -name "*${chr}.vcf" | sed "s/^/ --knownSites /g" | tr "\n" " " )
 recalparmsdbsnp=" -knownSites $dbsnp_local "
 
-realparms=$( find ${PWD} -name "*${chr}*.vcf" | sed "s/^/ -known /g" | tr "\n" " " )
+realparms=$( find ${PWD} -name "*${chr}.vcf" | sed "s/^/ -known /g" | tr "\n" " " )
 
 if [ `expr ${#recalparmsindels}` -lt 1 ]
 then
@@ -437,7 +437,7 @@ $javadir/java -Xmx16g  -Djava.io.tmpdir=$tmpdir -jar $gatkdir/GenomeAnalysisTK.j
 	 -stand_emit_conf 30 \
 	 --sample_ploidy $ploidy \
 	 -nt 1 -nct $thr \
-	 -L $chr
+	 -L $chr\
 	 -o $rawvariant
 
 
