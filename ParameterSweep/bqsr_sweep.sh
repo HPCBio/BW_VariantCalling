7bda98c8617589f5455d4c99683e130f82e9f756
Azza Ahmed <azzaea@uofk.edu>
2016-09-23 23:47:35 +0300

    BQSR sweep scripts

--- ParameterSweep/bqsr_sweep.sh
+++ ParameterSweep/bqsr_sweep.sh
@@ -1,215 +1,308 @@
 #!/bin/bash
 
-#This scrips performs a parameter sweep for the bqsr stage of a variant calling pipeline. Run it as: bqsr_sweep.sh <runfile>
+#This scrips performs a parameter sweep for the bqsr stage of a variant calling pipeline. Run it as: bqsr_sweep.sh <runfile> <sample> <chr> <log.in> <log.ou> <qsub>
 
-runfile=$1
 redmine=hpcbio-redmine@igb.illinois.edu
 
-#PBS -o localhost:$HOME/outputs-bqsr.log.txt
-#PBS -e localhost:$HOME/errors-bqsr.log.txt		
-
-if [ $# != 1 ]
+if [ $# != 6 ]
 then
-     MSG="Parameter mismatch.\nRerun like this: $0 <runfile>\n"     
+     MSG="Parameter mismatch.\nRerun like this: $0 <runfile> <sample> <chr> <log.in> <log.ou> <qsub>\n"     
      echo -e "program=$0 stopped at line=$LINENO. Reason=$MSG" | mail -s "ParameterSweep for the BQSR stage failed" "$redmine"
      exit 1;
 fi
 
-set -x
-######### Paths defintions:
-refdir=$( cat $runfile | grep -w REFGENOMEDIR | cut -d '=' -f2 )
-refgenome=$( cat $runfile | grep -w REFGENOME | cut -d '=' -f2 ) 
-ref_local=${refdir}/$refgenome
+echo -e "\n\n#####################################################################################"        
+echo -e "#############             BEGIN ANALYSIS PROCEDURE                    ###############"
+echo -e "#####################################################################################\n\n"        
 
-rootdir=$( cat $runfile | grep -w OUTPUTDIR | cut -d '=' -f2 )
-TopOutputLogs=$rootdir/logs
+echo -e "\n\n#####################################################################################"        
+echo -e "#############             DECLARING VARIABLES                         ###############"
+echo -e "#####################################################################################\n\n"        
 
-email=$( cat $runfile | grep -w EMAIL | cut -d '=' -f2 )
-reportticket=$( cat $runfile | grep -w REPORTTICKET | cut -d '=' -f2 )
-sampleinfo=$( cat $runfile | grep SAMPLEINFORMATION | cut -d '=' -f2)
+scriptfile=$0
+runfile=$1
+SampleName=$2
+chr=$3
+elog=$4
+olog=$5
+qsubfile=$6
+LOGS="jobid:${PBS_JOBID}\nqsubfile=$qsubfile\nerrorlog=$elog\noutputlog=$olog"
 
-javadir=$( cat $runfile | grep -w JAVADIR | cut -d '=' -f2 )
-
-reads="/home/groups/hpcbio_shared/azza/GIAB/reads"
-
-# Default run
-if [ ! -d  $rootdir/p4_indelRealign_bqsr/default ]; then
-	mkdir -p  $rootdir/p4_indelRealign_bqsr/bqsr/default
+if [ ! -s $runfile ] ; then
+	    MSG="$runfile runfile not found"
+	    echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | mail -s "Variant Calling Workflow failure message" "$redmine"
+	    exit 1;
 fi
 
-cd $rootdir/p4_indelRealign_bqsr/bqsr/default
+reportticket=$( cat $runfile | grep -w REPORTTICKET | cut -d '=' -f2 )
+email=$( cat $runfile | grep -w EMAIL | cut -d '=' -f2 )
+rootdir=$( cat $runfile | grep -w OUTPUTDIR | cut -d '=' -f2 )
+tmpdir=$( cat $runfile | grep -w TMPDIR | cut -d '=' -f2 ) 
+sampleinfo=$( cat $runfile | grep SAMPLEINFORMATION | cut -d '=' -f2)
+refdir=$( cat $runfile | grep -w REFGENOMEDIR | cut -d '=' -f2 )
+refgenome=$( cat $runfile | grep -w REFGENOME | cut -d '=' -f2 ) 
+javadir=$( cat $runfile | grep -w JAVADIR | cut -d '=' -f2 )
+gatkdir=$( cat $runfile | grep -w GATKDIR | cut -d '=' -f2 )
+samtoolsdir=$( cat $runfile | grep -w SAMDIR | cut -d '=' -f2 )
+rdir=$( cat $runfile | grep -w RDIR | cut -d '=' -f2 )
 
-module load gatk/3.6
-GenomeAnalysisTK=/home/apps/gatk/gatk-3.6/GenomeAnalysisTK.jar
+thr=$( cat $runfile | grep -w PBSCORES | cut -d '=' -f2 )
 
-module load R/3.2.3
+ref_local=${refdir}/$refgenome
+dbsnp_local=${refdir}/$dbSNP
+indel_local=${refdir}/$indeldir
+echo -e "\n\n##################################################################################" >&2 
+echo -e "##################################################################################" >&2    
+echo -e "#######   we will need these guys throughout, let's take care of them now   ######" >&2
+echo -e "##################################################################################" >&2 
+echo -e "##################################################################################\n\n" >&2
 
-echo Run using the default parameters: > "../../bqsr.summary.txt"
-echo "The parameters are:     ( ics maxCycle mcs bqsrBAQGOP ddq idq lqt mdq )">> "../../bqsr.summary.txt"
-echo "The default values are: ( 3     500     2      40     45   45  2  -1  )">> "../../bqsr.summary.txt"
-echo  "------------------------------------------------------------------------------------------------------------" >> "../../bqsr.summary.txt"
+outputdir=$rootdir/$SampleName
+AlignDir=$outputdir/align
+RealignDir=$outputdir/realign
+inputbam=$AlignDir/${SampleName}.wdups.sorted.bam                # name of the bam file that align-dedup produced
+dedupsortedbam=${SampleName}.$chr.wdups.sorted.bam               # name of the aligned file -one chr out of inputbam
 
-#if [ $run_default ]; then
-	START=$(date +%s)
-	java -jar $GenomeAnalysisTK\
-		-T BaseRecalibrator\
-		-R $ref_local\
-		-I $reads/project.NIST_NIST7035_H7AP8ADXX_TAAGGCGA_1_NA12878.bwa.markDuplicates.bam\
-		-L $reads/TruSeq_exome_targeted_regions.hg19.bed\
-		-knownSites $ref_local/dbsnp_138.hg19.vcf\
-	  	-knownSites $ref_local/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf\
-	 	-knownSites $ref_local/1000G_phase1.indels.hg19.sites.vcf\
-		-o recal.table.default.0
-	END=$(date +%s)
-	DIFF=$(( $END - $START ))
-	exitcode=$?
-	echo "exit code from BaseRecalibrator using: defaults 0 = $exitcode ; execution time = $DIFF" >> "../../bqsr.summary.txt"
+echo -e "\n\n##################################################################################"
+echo -e "########### PREP WORK: split aligned.bam by chr and grab indels for chr            ###" 
+echo -e "##################################################################################\n\n"
+
+cd $RealignDir
+
+$samtoolsdir/samtools view -bu -@ $thr -h $inputbam $chr > $dedupsortedbam
+exitcode=$?
+
+echo `date`
+if [ $exitcode -ne 0 ]; then
+	MSG="samtools command failed to split $inputbam by chr exitcode=$exitcode"
+	echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS"
+	exit $exitcode;
+fi
+if [ -s $dedupsortedbam ]; then
+    set +x         
+    echo -e "### the file was created. But we are not done.     #############"
+    echo -e "### sometimes we may have a BAM file with NO alignmnets      ###"
+    set -x
+    numAlignments=$( $samtoolsdir/samtools view -c $dedupsortedbam )
+
+    echo `date`
+    if [ $numAlignments -eq 0 ]
+    then
+        echo -e "${SampleName}\tREALIGNMENT\tFAIL\tsamtools command did not produce alignments for $dedupsortedbam\n" >> $qcfile
+        MSG="samtools command did not produce alignments for $dedupsortedbam"
+        echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | mail -s "[Task #${reportticket}]" "$redmine,$email"
+        exit 1;
+    else
+        set +x
+        echo -e "####### $dedupsortedbam seems to be in order ###########"
+        set -x 
+    fi
+else
+    MSG="samtools command did not produce a file $dedupsortedbam"
+    echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS" | mail -s "[Task #${reportticket}]" "$redmine,$email"
+    exit 1;
+fi
+
+echo -e "\n### index  $dedupsortedbam                                 ###\n"  
+
+$samtoolsdir/samtools index $dedupsortedbam
+exitcode=$?
+echo `date`
+if [ $exitcode -ne 0 ]
+then
+         MSG="samtools index command failed to split $inputbam by chr exitcode=$exitcode"
+         echo -e "program=$scriptfile stopped at line=$LINENO.\nReason=$MSG\n$LOGS"
+         exit $exitcode;
+fi
+set +x
+echo -e "\n\n### grab indels for this region. We need two variables for recalibration"
+echo -e "###  because they are specified differently for each GATK tool                            ##############\n\n"
+set -x
+
+cd $indel_local
+
+recalparmsindels=$( find ${PWD} -name "*${chr}.vcf" | sed "s/^/ --knownSites /g" | tr "\n" " " )
+recalparmsdbsnp=" -knownSites $dbsnp_local "
+
+if [ `expr ${#recalparmsindels}` -lt 1 ] 
+then
+	MSG="no indels were found for $chr in this folder $indel_local"
+	echo -e "program=$0 stopped at line=$LINENO. Reason=$MSG" | mail -s "[Task #${reportticket}]" "$redmine,$email"
+	exit 1;
+fi
+
+echo -e "\n\n##################################################################################"
+echo -e "################# Running BQSR: Default parameters            ##########################" 
+echo -e "\n\n##################################################################################"
+
+if [ ! -d  $rootdir/sweepBQSR/default ]; then
+	mkdir -p  $rootdir/sweepBQSR/default
+fi
+
+cd $rootdir/sweepBQSR/default
+
+module load R ################################################ R is assumed availble in the user's environment, along with some libraries. More details are on: https://software.broadinstitute.org/gatk/guide/article?id=2899
+
+echo Run using the default parameters: > "../bqsr.summary.txt"
+echo "The parameters are:     ( ics maxCycle mcs bqsrBAQGOP ddq idq lqt mdq )">> "../bqsr.summary.txt"
+echo "The default values are: ( 3     500     2      40     45   45  2  -1  )">> "../bqsr.summary.txt"
+echo  "------------------------------------------------------------------------------------------------------------" >> "../bqsr.summary.txt"
+
+START=$(date +%s)
+$javadir/java -Xmx16g  -Djava.io.tmpdir=$tmpdir -jar $gatkdir/GenomeAnalysisTK.jar\
+	-T BaseRecalibrator\
+	-R $ref_local\
+	-I $dedupsortedbam\
+	$recalparmsindels \
+       	$recalparmsdbsnp \
+	--out $SampleName.$chr.recal_table.default.0 \
+	-nct $thr
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+exitcode=$?
+echo "exit code from BaseRecalibrator chr=$chr using: defaults 0 = $exitcode ; execution time = $DIFF" >> "../bqsr.summary.txt"
 	
-	START=$(date +%s)
-	java -jar $GenomeAnalysisTK\
-	       	-T BaseRecalibrator\
-	        -R $ref_local\
-		-I $reads/project.NIST_NIST7035_H7AP8ADXX_TAAGGCGA_1_NA12878.bwa.markDuplicates.bam\
-	        -L $reads/TruSeq_exome_targeted_regions.hg19.bed\
-	        -knownSites $ref_local/dbsnp_138.hg19.vcf\
-	        -knownSites $ref_local/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf\
-	        -knownSites $ref_local/1000G_phase1.indels.hg19.sites.vcf\
-	        -BQSR recal.table.default.0\
-	        -o after_recal.table.default.0
-	END=$(date +%s)
-	DIFF=$(( $END - $START ))
-	exitcode=$?
-	echo "exit code from the rerun of BaseRecalibrator using: defaults 0 = $exitcode  ; execution time = $DIFF" >> "../../bqsr.summary.txt"
+START=$(date +%s)
+java -Xmx16g  -Djava.io.tmpdir=$tmpdir -jar $gatkdir/GenomeAnalysisTK.jar\
+       	-T BaseRecalibrator\
+        -R $ref_local\
+	-I $dedupsortedbam\
+        $recalparmsindels \
+	$recalparmsdbsnp \
+        -BQSR $SampleName.$chr.recal_table.default.0\
+        -o $SampleName.$chr.after_recal_table.default.0\
+	-nct $thr
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+exitcode=$?
+echo "exit code from the rerun of BaseRecalibrator chr=$chr using: defaults 0 = $exitcode  ; execution time = $DIFF" >> "../bqsr.summary.txt"
 
-	START=$(date +%s)
-	java -jar $GenomeAnalysisTK\
-		-T AnalyzeCovariates\
-		-R $ref_local\
-		-L $reads/TruSeq_exome_targeted_regions.hg19.bed\
-		-before recal.table.default.0\
-		-after after_recal.table.default.0\
-		-plots recalibration_plots.default.0.pdf
-	END=$(date +%s)
-	DIFF=$(( $END - $START ))
-	exitcode=$?
-	echo "exit code from AnalyzeCovariates using: defaults 0 = $exitcode  ; execution time = $DIFF" >> "../../bqsr.summary.txt"
+START=$(date +%s)
+java -Xmx16g  -Djava.io.tmpdir=$tmpdir -jar $gatkdir/GenomeAnalysisTK.jar\
+	-T AnalyzeCovariates\
+	-R $ref_local\
+	-before $SampleName.$chr.recal_table.default.0\
+	-after $SampleName.$chr.after_recal_table.default.0\
+	-plots $SampleName.$chr.recalibration_plots.default.0.pdf
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+exitcode=$?
+echo "exit code from AnalyzeCovariates chr=$chr using: defaults 0 = $exitcode  ; execution time = $DIFF" >> "../bqsr.summary.txt"
+		
+START=$(date +%s)
+java -Xmx16g  -Djava.io.tmpdir=$tmpdir -jar $gatkdir/GenomeAnalysisTK.jar\
+	-T PrintReads\
+	-R $ref_local\
+	-I $dedupsortedbam\
+	-BQSR $SampleName.$chr.recal_table.default.0\
+	-o $SampleName.$chr.recal.default.0.bam\
+	-nct $thr
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+exitcode=$?
+echo "exit code from PrintReads chr=$chr using: defaults 0 = $exitcode  ; execution time = $DIFF" >> "../bqsr.summary.txt"
 	
-	
-	START=$(date +%s)
-	java -jar $GenomeAnalysisTK\
-		-T PrintReads\
-		-R $ref_local\
-		-I $reads/project.NIST_NIST7035_H7AP8ADXX_TAAGGCGA_1_NA12878.bwa.markDuplicates.bam\
-		-L $reads/TruSeq_exome_targeted_regions.hg19.bed\
-		-BQSR recal.table.default.0\
-		-o recal.default.0.bam
-	END=$(date +%s)
-	DIFF=$(( $END - $START ))
-	exitcode=$?
-	echo "exit code from PrintReads using: defaults 0 = $exitcode  ; execution time = $DIFF" >> "../../bqsr.summary.txt"
-	
-	echo >> "../../bqsr.summary.txt"
-	echo "####################################################################################################" >> ../../bqsr.summary.txt
-#fi
-
-declare -a parameters=(ics maxCycle mcs bqsrBAQGOP ddq idq lqt mdq)
-declare -a min=(1 250 1 10 10 10 1 2)
-declare -a step=(2 150 2 10 10 10 2 2)
-declare -a max=(13 1000 13 70 70 70 12 12) 
+echo >> "../bqsr.summary.txt"
+echo "####################################################################################################" >> ../bqsr.summary.txt
 
 
-cd $rootdir/p4_indelRealign_bqsr/bqsr
+declare -a parameters=(ics maxCycle mcs bqsrBAQGOP ddq idq lqt mdq) 
+declare -a min=(1 250 1 10 10 10 1 2) 
+declare -a step=(2 150 2 10 10 10 2 2) 
+declare -a max=(13 1000 13 70 70 70 12 12)
+
+
+cd $rootdir/sweepBQSR
 mkdir ${parameters[@]}
 
-echo The parameters being tested and their ranges are given below: >> "../bqsr.summary.txt"
-echo paramters: ${parameters[@]}, >> "../bqsr.summary.txt"
-echo minimum  : ${min[@]} >> "../bqsr.summary.txt"
-echo maximum  : ${max[@]} >> "../bqsr.summary.txt"
-echo >> "../bqsr.summary.txt"
-echo  "------------------------------------------------------------------------------------------------------------" >> ../"bqsr.summary.txt"
+echo The parameters being tested and their ranges are given below: >> "bqsr.summary.txt"
+echo paramters: ${parameters[@]}, >> "bqsr.summary.txt"
+echo minimum  : ${min[@]} >> "bqsr.summary.txt"
+echo maximum  : ${max[@]} >> "bqsr.summary.txt"
+echo >> "bqsr.summary.txt"
+echo  "------------------------------------------------------------------------------------------------------------" >> "bqsr.summary.txt"
 
 pos=0
-echo start while loop ; 
 
-while [ $pos -lt ${#parameters[@]} ]
-do
-	echo inside while
-	module load gatk/3.6
-	GenomeAnalysisTK=/home/apps/gatk/gatk-3.6/GenomeAnalysisTK.jar
-	module load R/3.2.3
+while [ $pos -lt ${#parameters[@]} ]; do
 	par=${parameters[pos]}
-	cd $rootdir/p4_indelRealign_bqsr/bqsr/$par
+	cd $rootdir/sweepBQSR/$par
 	for i in $(seq ${min[pos]} ${step[pos]} ${max[pos]}); do
 		START=$(date +%s)
-		java -jar $GenomeAnalysisTK\
+		java -Xmx16g  -Djava.io.tmpdir=$tmpdir -jar $gatkdir/GenomeAnalysisTK.jar \
 		        -T BaseRecalibrator\
 		        -R $ref_local\
-			-I $reads/project.NIST_NIST7035_H7AP8ADXX_TAAGGCGA_1_NA12878.bwa.markDuplicates.bam\
-		        -L $reads/TruSeq_exome_targeted_regions.hg19.bed\
-		        -knownSites $ref_local/dbsnp_138.hg19.vcf\
-		        -knownSites $ref_local/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf\
-		        -knownSites $ref_local/1000G_phase1.indels.hg19.sites.vcf\
+			-I $dedupsortedbam\
+                        $recalparmsindels \
+                	$recalparmsdbsnp \
 			-$par "$i"\
-		        -o recal.table.$par.$i
+		        -o $SampleName.$chr.recal_table.$par.$i\
+			-nct $thr
 		END=$(date +%s)
 		DIFF=$(( $END - $START ))
 		exitcode=$?
 		echo "exit code from BaseRecalibrator using: -$par $i = $exitcode ; execution time = $DIFF" >> "../bqsr.summary.txt"
 
-		if [ -s "recal.table.$par.$i" ]; then
-			echo 'successful parameter test $par=$i'
+		if [ -s "$SampleName.$chr.recal_table.$par.$i" ]; then
+			echo 'successful parameter test chr=$chr $par=$i'
 		else
-			echo "BQSR failed using -$par $i">> "../bqsr.summary.txt"
+			echo "BQSR chr=$chr failed using -$par $i">> "../bqsr.summary.txt"
 			echo "Execution time is :$DIFF: seconds" >> "../bqsr.summary.txt"
 			echo
 			continue
 		fi
 			
 		START=$(date +%s)
-		java -jar $GenomeAnalysisTK\
+		java -Xmx16g  -Djava.io.tmpdir=$tmpdir -jar $gatkdir/GenomeAnalysisTK.jar\
 		        -T BaseRecalibrator\
 		        -R $ref_local\
-			-I $reads/project.NIST_NIST7035_H7AP8ADXX_TAAGGCGA_1_NA12878.bwa.markDuplicates.bam\
-		        -L $reads/TruSeq_exome_targeted_regions.hg19.bed\
-		        -knownSites $ref_local/dbsnp_138.hg19.vcf\
-		        -knownSites $ref_local/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf\
-		        -knownSites $ref_local/1000G_phase1.indels.hg19.sites.vcf\
-		        -BQSR recal.table.$par.$i\
-		        -o after_recal.table.$par.$i
+			-I $dedupsortedbam\
+                        $recalparmsindels \
+                	$recalparmsdbsnp \
+		        -BQSR $SampleName.$chr.recal_table.$par.$i\
+		        -o $SampleName.$chr.after_recal_table.$par.$i\
+			-nct $thr
 		END=$(date +%s)
 		DIFF=$(( $END - $START ))
 		exitcode=$?
 	        echo "exit code from the rerun of  BaseRecalibrator with: -$par $i = $exitcode ; execution time = $DIFF" >> "../bqsr.summary.txt"
 		
 		START=$(date +%s)
-		java -jar $GenomeAnalysisTK\
+		java -Xmx16g  -Djava.io.tmpdir=$tmpdir -jar $gatkdir/GenomeAnalysisTK.jar\
 		        -T AnalyzeCovariates\
 		        -R $ref_local\
-		        -L $reads/TruSeq_exome_targeted_regions.hg19.bed\
-		        -before recal.table.$par.$i\
-		        -after after_recal.table.$par.$i\
-		        -plots recalibration_plots.$par.$i.pdf
+		        -before $SampleName.$chr.recal_table.$par.$i\
+		        -after $SampleName.$chr.after_recal_table.$par.$i\
+		        -plots $SampleName.$chr.recalibration_plots.$par.$i.pdf
 		END=$(date +%s)
 		DIFF=$(( $END - $START ))
 		exitcode=$?
 	        echo "exit code from AnalyzeCovariates using $par $i = $exitcode; execution time = $DIFF" >> "../bqsr.summary.txt"
 
 		START=$(date +%s)
-		java -jar $GenomeAnalysisTK\
+		java -Xmx16g  -Djava.io.tmpdir=$tmpdir -jar $gatkdir/GenomeAnalysisTK.jar\
 		        -T PrintReads\
 		        -R $ref_local\
-			-I $reads/project.NIST_NIST7035_H7AP8ADXX_TAAGGCGA_1_NA12878.bwa.markDuplicates.bam\
-		        -L $reads/TruSeq_exome_targeted_regions.hg19.bed\
-		        -BQSR recal.table.$par.$i\
-		        -o recal.$par.$i.bam
+			-I $dedupsortedbam\
+		        -BQSR $SampleName.$chr.recal_table.$par.$i\
+		        -o $SampleName.$chr.recal.$par.$i.bam\
+			-nct $thr
 		END=$(date +%s)
 		DIFF=$(( $END -$START + $DIFF ))
 		exitcode=$?
-		echo "exit code from PrintReads with: -$par $i = $exitcode  ; execution time = $DIFF" >> "../bqsr.summary.txt"
+		echo "exit code from PrintReads chr=$chr with: -$par $i = $exitcode  ; execution time = $DIFF" >> "../bqsr.summary.txt"
 		echo  >> "../bqsr.summary.txt"
 	done
 let pos+=1
 done
+
+echo -e "\n\n########################################################################################"
+echo -e "##############                 plotting now for this chr=$chr                            ##################"	
+echo -e "########################################################################################\n\n"
+
+
+Rscript bqsrplotting.R $rootdir/sweepBQSR/ $thr
 
 
 
--- ParameterSweep/bqsrplotting.R
+++ ParameterSweep/bqsrplotting.R
@@ -1,3 +1,8 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+#User need to supply the following: the path to the swept files, the number of cores for parallelization
+
 library(gsalib)
 library(dplyr)
 if (!require(doParallel)){
@@ -26,18 +31,15 @@ parameters = data.frame(
 )
 defaults = c(40, 45, 3, 45, 2, 500, 2, -1)
 
-setwd("/home/groups/hpcbio_shared/azza/GIAB/results/p4_indelRealign_bqsr/bqsr/default")  
-before = Sys.glob('recal.table.*')
-after = Sys.glob('after_recal.*')
+setwd(paste0(args[1],"/default")) #go to the default folder
+before = Sys.glob('*recal.table.*')
+after = Sys.glob('*after_recal.*')
 db = gsa.read.gatkreport(before)
 qb = db$RecalTable1
 da = gsa.read.gatkreport(after)
 qa = da$RecalTable1
-fwse_before.default = sum((qb$EmpiricalQuality - 
-                     qb$QualityScore)^2*qb$Observations/sum(as.numeric(qb$Observations)),na.rm = T)
-fwse_after.default = sum((qa$EmpiricalQuality - 
-                    qa$QualityScore)^2*qa$Observations/sum(as.numeric(qa$Observations)), na.rm = T)
-
+fwse_before.default = sum((qb$EmpiricalQuality - qb$QualityScore)^2*qb$Observations/sum(as.numeric(qb$Observations)),na.rm = T)
+fwse_after.default = sum((qa$EmpiricalQuality - qa$QualityScore)^2*qa$Observations/sum(as.numeric(qa$Observations)), na.rm = T)
 
 dirs = list.dirs('../')  
 dirs = dirs[-which(dirs=='..//default')]
@@ -46,7 +48,7 @@ param_name = as.character(parameters[parameters[[1]]==param,2])
 ####################################################################
 
    #registerDoMC()
-cluster = makeCluster(20)
+cluster = makeCluster(args[2]) #Second argument is number of core to use
    #registerDoSNOW(cluster)
 registerDoParallel(cluster)
 
@@ -55,11 +57,11 @@ foreach (i = 1:length(dirs)+1 ) %dopar% {
 #for (i in seq_along(dirs)+1 ) {
   library(gsalib)
   library(dplyr)
-  pdffile=paste0('/home/groups/hpcbio_shared/azza/GIAB/results/p4_indelRealign_bqsr/bqsr/bqsr_effect_',param_name[i-1],'.pdf')
+  pdffile=paste0(args[1],'bqsr_effect_',param_name[i-1],'.pdf')
   pdf(pdffile,title = 'Effect of parameter chages in the bqsr result')
   setwd(dirs[i])
-  before = Sys.glob('recal.table.*')
-  after = Sys.glob('after_recal.*')
+  before = Sys.glob('*recal.table.*')
+  after = Sys.glob('*after_recal.*')
 
   values=c()
   fwsebefore=c()
@@ -75,10 +77,8 @@ foreach (i = 1:length(dirs)+1 ) %dopar% {
       qb = db$RecalTable1  #The bqsr table of the reported quality as covariate
       da = gsa.read.gatkreport(after[valuea])
       qa = da$RecalTable1
-      fwse_before = sum((qb$EmpiricalQuality - 
-                           qb$QualityScore)^2*qb$Observations/sum(as.numeric(qb$Observations)),na.rm = T)
-      fwse_after = sum((qa$EmpiricalQuality - 
-                          qa$QualityScore)^2*qa$Observations/sum(as.numeric(qa$Observations)), na.rm = T)
+      fwse_before = sum((qb$EmpiricalQuality - qb$QualityScore)^2*qb$Observations/sum(as.numeric(qb$Observations)),na.rm = T)
+      fwse_after = sum((qa$EmpiricalQuality - qa$QualityScore)^2*qa$Observations/sum(as.numeric(qa$Observations)), na.rm = T)
       values = c(values, valueb)
       fwsebefore = c(fwsebefore, fwse_before)
       fwseafter = c(fwseafter, fwse_after)
@@ -88,18 +88,18 @@ foreach (i = 1:length(dirs)+1 ) %dopar% {
   colnames(FWSE) = c('value','before','after')
   FWSE$value = as.double(as.character(FWSE$value))
   FWSE = arrange(FWSE,value)
-  filename=paste0("/home/groups/hpcbio_shared/azza/GIAB/results/p4_indelRealign_bqsr/bqsr/fwsevalues",param_name[i-1],".txt")
+  filename=paste0(args[1],"/fwsevalues",param_name[i-1],".txt")
   write.table(FWSE,filename,sep='\t',append=TRUE, col.names=TRUE)
   ## plot fwse changes Vs parameter changes; i.e as you vary each parameter, 
   #show what happens to fwse
   # actually, you need to know the relative improvement in quality scores after bqsr, 
   # which is |fwse_before -  fwse_after|
   par(xpd=T, mar=c(5,4,4,6))
-  plot(FWSE$value, FWSE$before-FWSE$after, 
+  plot(FWSE$value, abs(FWSE$before-FWSE$after), 
        main = paste('FWSE after in response to \n "',param_name[i-1],'" change'),
        xlab = param[i-1], ylab = '|change in QUAL|')
-  lines(FWSE$value, FWSE$before-FWSE$after)
-  points(defaults[i-1],fwse_before.default-fwse_after.default, pch = 19, col ='red')
+  lines(FWSE$value, abs(FWSE$before-FWSE$after))
+  points(defaults[i-1],abs(fwse_before.default-fwse_after.default), pch = 19, col ='red')
   legend('topleft',c('default'), pch = 19,
          col=c('red'),inset=c(1.03,0), cex = 0.6 )
   dev.off()
